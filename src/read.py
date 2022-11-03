275426cb02ec5e3ebc7a865b0f981b373df70a42
Anansi86 <shawn.staton@student.awesomeinc.org>
2022-11-03 23:33:53 +0000

    join

--- src/read.py
+++ src/read.py
@@ -1,22 +1,36 @@
 from database.connection import execute_query
 
+def view_all():
+    query = """
+    SELECT heroes.id, heroes.name, heroes.about_me, heroes.biography, ability_types.name
+    FROM heroes
+    LEFT OUTER JOIN abilities
+    ON heroes.id = abilities.hero_id
+    LEFT OUTER JOIN ability_types
+    ON abilities.ability_type_id = ability_types.id
+    """
+    list_of_heroes = execute_query(query).fetchall()
+    print(list_of_heroes)
+    
+view_all()
+
 #heroes = execute_query("""
 #SELECT *
 #FROM ability_types
 #""")
 #print(list(heroes))
 
-def select_all():
-    query = """
-        SELECT * from ability_types
-    """
+#def select_all():
+#    query = """
+#        SELECT * from ability_types
+#    """
 
-    list_of_heroes = execute_query(query).fetchall()
-    print(list_of_heroes)
-    for record in list_of_heroes:
-         print(record[1])
+#    list_of_heroes = execute_query(query).fetchall()
+#    print(list_of_heroes)
+#    for record in list_of_heroes:
+#         print(record[1])
 
-select_all()
+#select_all()
 
 #heroes = execute_query("""
 #SELECT * FROM heroes
